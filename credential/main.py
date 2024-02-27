4486cfc61c66268392524548de1fd94068cdf1a2
DylanChenZ <149108255+DylanChenZ@users.noreply.github.com>
2024-02-27 10:20:02 -0500

    Update main.py

--- credential/main.py
+++ credential/main.py
@@ -1,8 +1,10 @@
 import pickle
 import re
 from tkinter import *
+import json
 import os
 import time
+
 
 def center(win):
     win.update_idletasks()
@@ -16,9 +18,10 @@ def center(win):
     y = win.winfo_screenheight() // 2 - win_height // 2
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     win.deiconify()
-    
+
+
 class login_page(Toplevel):
-    def __init__(self,master):
+    def __init__(self, master):
         super().__init__(master)
         self.geometry("600x400")
         self.title("register")
@@ -28,46 +31,46 @@ class login_page(Toplevel):
         self.password = StringVar()
         self.user_name = StringVar()
         self.page = self.create()
-        
-    
+
     def create(self):
         Label(self, text="login_page", font=("calibre", 25), bg="grey").pack()
         Label(self, text=" ", height="4").pack()
         Label(self, text="user_name ", font=("calibre", 20)).pack()
         self.entry_user_name = Entry(self, textvariable=self.user_name).pack()
-        self.good_username= Label(self, text="", fg='#f00')
-        self.good_username.pack()  
+        self.good_username = Label(self, text="", fg='#f00')
+        self.good_username.pack()
         Label(self, text=" ", height="1", width="30").pack()
         Label(self, text="password", font=("calibre", 20)).pack()
         self.entry_password = Entry(self, textvariable=self.password).pack()
-        self.good_password = Label(self, text = "", fg = "#f00")
+        self.good_password = Label(self, text="", fg="#f00")
         self.good_password.pack()
-        Button(self, text="login", command= self.check_username).pack()
-        
+        Button(self, text="login", command=self.check_username).pack()
+
     def check_username(self):
         if not re.search(".*@.*[.]", self.user_name.get()) or self.user_name.get() not in info:
-            self.good_username.config(text = "provide a valid email please / exist email")  
+            self.good_username.config(text="provide a valid email please / exist email")
         else:
-            self.good_username.config(text = "")
+            self.good_username.config(text="")
             self.check_password()
-        
+
     def check_password(self):
         if self.password.get() != info[self.user_name.get()]:
-            self.good_password.config(text = "wrong password")
+            self.good_password.config(text="wrong password")
         else:
-            self.good_password.config(text = "")
+            self.good_password.config(text="")
             self.login_success()
-            
+
     def login_success(self):
-        Label(self, text = "login successful!", fg = "green").pack()
+        Label(self, text="login successful!", fg="green").pack()
         self.update()
         time.sleep(2)
         self.close()
-        
+
     def close(self):
         self.destroy()
         self.update()
-        
+
+
 class register_page(Toplevel):
     def __init__(self, master):
         super().__init__(master)
@@ -76,14 +79,14 @@ class register_page(Toplevel):
         x = master.winfo_x()
         y = master.winfo_y()
         self.geometry("+%d+%d" % (x - 10, y - 10))
-        
+
         # Keep the toplevel window in front of the root window
         self.wm_transient(root)
         self.password = StringVar()
         self.user_name = StringVar()
         self.Page = self.create()
-        
-    def create(self):    
+
+    def create(self):
 
         Label(self, text="register page", font=("calibre", 25), bg="grey").pack()
         Label(self, text=" ", height="4").pack()
@@ -96,32 +99,37 @@ class register_page(Toplevel):
         self.entry_password = Entry(self, textvariable=self.password).pack()
         self.good_password = Label(self, text="", fg="#f00")
         self.good_password.pack()
-        Button(self, text="register", command= self.create_username).pack()
-        
+        Button(self, text="register", command=self.create_username).pack()
+
     def create_username(self):
         if not re.search(".*@.*[.]", self.user_name.get()):
-            self.good_username.config(text = "provide a valid email please")
-            
+            self.good_username.config(text="provide a valid email please")
         else:
-            self.good_username.config(text = "")
+            self.good_username.config(text="")
             self.create_password()
 
     def create_password(self):
         if not re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', self.password.get()):
-            self.good_password.config(text = "provide a good password please")
+            self.good_password.config(text="provide a good password please")
+        elif self.password.get() in info.values():
+            self.temp = "bruh"
+            for key, value in info.items():
+                if str(value) == str(self.password.get()):
+                    self.temp = str(key) + "has already took the password lol"
+            self.good_password.config(text=self.temp)
         else:
-            self.good_password.config(text = "")
+            self.good_password.config(text="")
             self.save_password()
-            
+
     def save_password(self):
         info[self.user_name.get()] = self.password.get()
         with open(filename, "wb") as f:
-            pickle.dump(info,f)
-        Label(self, text = "register successful!", fg = "green").pack()
+            pickle.dump(info, f)
+        Label(self, text="register successful!", fg="green").pack()
         self.update()
         time.sleep(2)
         self.close()
-        
+
     def close(self):
         self.destroy()
         self.update()
@@ -134,27 +142,27 @@ class Main_Page(Tk):
         center(self)
         self.title("credential")
         Label(text=" ", height="2", width="600", bg="grey").pack()
-        register_button = Button(text="register", height="5", width="15", command = self.create_register)
+        register_button = Button(text="register", height="5", width="15", command=self.create_register)
         register_button.place(x=240, y=220)
-        login_button = Button(text = "login", height = "5", width = "15", command = self.create_login)
-        login_button.place(x = 240, y = 100)
-        close_button = Button(text = "close", height="1", width="7", command = self.close)
-        close_button.place(x = 270, y = 325)
-        
+        login_button = Button(text="login", height="5", width="15", command=self.create_login)
+        login_button.place(x=240, y=100)
+        close_button = Button(text="close", height="1", width="7", command=self.close)
+        close_button.place(x=270, y=325)
+
     def create_register(self):
         self.register = register_page(self)
-    
+
     def create_login(self):
         self.login = login_page(self)
 
     def close(self):
         self.destroy()
         self.update()
-        with open("output.txt","w") as f:
+        with open("output.txt", "w") as f:
             for user_name, password in info.items():
-                f.write("user_name: %s \npassword: %s\n\n"%(user_name, password))
-        
-        
+                f.write("user_name: %s \npassword: %s\n\n" % (user_name, password))
+
+
 def main():
     global filename
     global info
@@ -170,9 +178,4 @@ def main():
 
 
 if __name__ == "__main__":
-   main()
-
-
-
-
-
+    main()
